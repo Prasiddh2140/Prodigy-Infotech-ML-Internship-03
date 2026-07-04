0a895efa7a7078f45a399d81148474146aa340fb
Prasiddh Narayan Singh <prasiddhsingh525@gmail.com>
2026-07-04 20:10:56 +0530

    did changes

--- svm_cat_dog.py
+++ svm_cat_dog.py
@@ -1,3 +1,4 @@
+import random
 import os
 import cv2
 import numpy as np
@@ -71,3 +72,54 @@ def load_images(folder, limit=None):
             y.append(1)
 
     return np.array(X), np.array(y)
+def load_images(folder, limit=None):
+    X = []
+    y = []
+
+    image_files = os.listdir(folder)
+
+    if limit is not None:
+         random.seed(42)
+         image_files = random.sample(image_files, limit)
+
+    for image_name in tqdm(image_files):
+
+        image_path = os.path.join(folder, image_name)
+
+        image = cv2.imread(image_path)
+
+        if image is None:
+            continue
+
+        # Convert to grayscale
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Resize image
+        image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+
+        # Flatten into a 1D feature vector
+        image = image.flatten()
+
+        X.append(image)
+
+        # Assign labels
+        if image_name.startswith("cat"):
+            y.append(0)
+        elif image_name.startswith("dog"):
+            y.append(1)
+
+    return np.array(X), np.array(y)
+X, y = load_images(TRAIN_DIR, limit=2000)
+print("Feature Matrix Shape :", X.shape)
+print("Labels Shape :", y.shape)
+print(np.unique(y))
+unique, counts = np.unique(y, return_counts=True)
+
+for label, count in zip(unique, counts):
+    animal = "Cat" if label == 0 else "Dog"
+    print(f"{animal}: {count}")
+    plt.figure(figsize=(4,4))
+plt.imshow(X[0].reshape(64,64), cmap="gray")
+plt.title(f"Label: {'Cat' if y[0]==0 else 'Dog'}")
+plt.axis("off")
+plt.show()
